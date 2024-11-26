5424a09e1eda91e87cddcfb9fa0b1641796505fb
DevZeEa <emailtozia0@gmail.com>
2024-11-26 07:34:36 +0500

    after adding footer

--- src/app/(client)/components/Footer.jsx
+++ src/app/(client)/components/Footer.jsx
@@ -1,32 +1,39 @@
+import { K2D } from 'next/font/google'
+import Link from 'next/link'
+const k2d = K2D({
+    subsets: ['latin'],
+    weight: ['400']
+});
 function Footer() {
     return (
         <>
 
 
-<footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+<footer className="bg-white rounded-lg shadow dark:bg-[#1b1b1b] m-4">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
+        <div className="
+        flex flex-col gap-5 sm:items-center sm:justify-between">
+        <div className={`logo uppercase text-lg md:text-2xl mx-auto md:m-0 custom font-bold font-[${k2d.style.fontFamily}]`}><Link href='/'>Logo</Link></div>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                    <a href="#" className="hover:underline me-4 md:me-6">Home</a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    <a href="#" className="hover:underline me-4 md:me-6">Services</a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                    <a href="#" className="hover:underline me-4 md:me-6">About me</a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline">Contact</a>
+                    <a href="#" className="hover:underline">Potfolio</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact me</a>
                 </li>
             </ul>
+
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+
     </div>
 </footer>
 
